90e778a22c7719b7a26d522a65a7dffe3d46d72c
하치 <franc2sca@naver.com>
2025-06-11 01:12:00 +0900

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,20 @@
+def calculate_factorial(num):
+    if num < 0:
+        return "음수는 팩토리얼을 계산할 수 없습니다."
+    elif num == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, num + 1):
+            result *= i
+        return result
+
+try:
+    input_num = int(input("팩토리얼을 계산할 정수를 입력하세요."))
+    factorial_result = calculate_factorial(input_num)
+    if isinstance(factorial_result, str):
+        print(factorial_result)
+    else:
+        print(f"{input_number}의 팩토리얼은 {factorial_result}입니다.")
+except ValueError:
+    print("유효한 정수를 입력해 주세요.")
